b87629645fb0f5533be25d3cbf51e8b4d6d23cd4
josafamarengo <josafabmarengo@gmail.com>
2022-07-05 16:40:14 -0300

    fix: setup configure

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,25 @@
-module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-    jest: true
+export const env = {
+  browser: true,
+  es2021: true,
+  node: true,
+  jest: true
+};
+export const extendss = ['eslint:recommended', 'plugin:prettier/recommended', 'airbnb-base'];
+export const parser = '@typescript-eslint/parser';
+export const parserOptions = {
+  project: 'tsconfig.json',
+  tsconfigRootDir: __dirname,
+  ecmaFeatures: {
+    jsx: true
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'airbnb-typescript',
-    'prettier'
-  ],
-  parser: '@typescript-eslint/parser',
-  files: ['*.ts', '*.tsx'],
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    ecmaFeatures: {
-      jsx: true
-    },
-    ecmaVersion: 12,
-    sourceType: 'module'
-  },
-  plugins: ['react', '@typescript-eslint', 'prettier'],
-  rules: {
-    'react/jsx-props-no-spreading': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'prettier/prettier': 'error',
-    'space-before-function-paren': 'off',
-    'react/prop-types': 'off'
-  }
+  ecmaVersion: 12,
+  sourceType: 'module'
+};
+export const plugins = ['react', '@typescript-eslint', 'prettier'];
+export const rules = {
+  'react/jsx-props-no-spreading': 'off',
+  'react/react-in-jsx-scope': 'off',
+  'prettier/prettier': 'error',
+  'space-before-function-paren': 'off',
+  'react/prop-types': 'off'
 };
